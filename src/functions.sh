af394668b0d0816125fad95cfe9dca0ccc73e97b
Aaron Faanes <dafrito@gmail.com>
2021-09-30 00:10:10 -0500

    Let tmuxify be run in a subdirectory of a tmuxify configuration
    TMUXIFY_CONF and SITE_PANES be deduced if not initially found

--- src/functions.sh
+++ src/functions.sh
@@ -43,7 +43,19 @@ TMUX=/usr/bin/tmux
 if test -e $TMUXIFY_CONF; then
      source $TMUXIFY_CONF || die "Failed to load tmuxify configuration"
 elif test "$TMUXIFY_CONF" != `pwd`/tmuxify.sh; then
-    die "Custom TMUXIFY_CONF was not found (TMUXIFY_CONF=$TMUXIFY_CONF)"
+    die "Custom TMUXIFY_CONF was not found (TMUXIFY_CONF=$TMUXIFY_CONF, `pwd`/tmuxify.sh)"
+else
+    # Default tmuxify configuration not found; move to parent directory
+    pushd . >/dev/null || die "Failed to pushd during tmuxify configuration"
+    while test `pwd` != '/'; do
+        if test -e tmuxify.sh; then
+            export TMUXIFY_CONF=`pwd`/tmuxify.sh
+            source $TMUXIFY_CONF || die "Failed to load tmuxify configuration"
+            break
+        fi
+        cd .. || break
+    done
+    popd >/dev/null || die "Failed to popd after searching for tmuxify configuration"
 fi
 test -n "$TMUX" || die "TMUX must be defined."
 test -e $TMUX || die "tmux executable not found (TMUX=$TMUX)"
@@ -59,11 +71,23 @@ if test -z "$SITE_URL"; then
     SITE_URL="http://`hostname`"
 fi
 test -d $SITE_PATH || die "SITE_PATH must be a directory (SITE_PATH=$SITE_PATH)."
-if ! test -n "$SITE_PANES"; then
-    export SITE_PANES=$SITE_PATH/panes
+if test -z "$SITE_PANES"; then
+    pushd . >/dev/null || die "Failed to find site panes"
+    while test `pwd` != '/'; do
+        # Stop if panes directory is found and we can use this directory as root
+        if test -d panes; then
+            SITE_PATH=`pwd`
+            SITE_NAME=`basename $SITE_PATH`
+            SITE_PANES=$SITE_PATH/panes
+            break;
+        fi
+        cd ..
+    done
+    popd >/dev/null || die "Failed to find site panes"
 fi
-test -d "$SITE_PANES" || die "SITE_PANES must be a directory (SITE_PANES=$SITE_PANES)"
+test -d $SITE_PANES || die "SITE_PANES must be a directory (SITE_PANES=$SITE_PANES)"
 
 if test -z "`echo $SITE_PANES/*`"; then
     die "No site panes found (SITE_PANES=$SITE_PANES)"
 fi
+export SITE_PANES
